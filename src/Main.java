28b11af730370ed7020f732d220383b4bf3efde5
DoChuan13 <chuando.9213@gmail.com>
2023-03-19 23:17:11 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import decToBin.DecToBin;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         DecToBin.convertDecToBin(scanner);
--- src/decToBin/DecToBin.java
+++ src/decToBin/DecToBin.java
@@ -11,12 +11,13 @@ public class DecToBin {
         int number = Integer.parseInt(scanner.nextLine());
         do {
             //Phép chia lấy phần dư để đưa vào Stack, vòng lặp dừng lại khi phép chia phần nguyên về = 0
-            int mod = number % 2;
-            decToBin.push(mod);
-            number /= 2;
-        } while (number != 0);
-        int sizeStack = decToBin.size();
+            int mod = number % 2;//lấy phần dư của phép chia cho 2 để push vào Stack bên dưới
+            decToBin.push(mod);//nhận lấy phần dư ở phép chia phía trên và push vào
+            number /= 2;//Gán lại number = phần nguyên của phép chia cho 2 để dùng cho vòng lặp tiếp theo
+        } while (number != 0);//kiểm tra điều kiện để dừng vòng lặp khi number trở về= 0
+        int sizeStack = decToBin.size();//kiểm tra kích thước của stack, dùng cho vòng lặp for phía dưới
         for (int i = 0; i < sizeStack; i++) {
+            //Pop để lấy ra lần lượt từng phần tử trong stack
             //Nối danh sách số dư theo chiều ngược lại sẽ gia giá trị nhị phân
             int numberBin = decToBin.pop();
             bin += numberBin;
